cb4214a2087583bdde2471d7830a4ab387cda983
kashif <kashifmd123@gmail.com>
2021-09-18 14:08:49 +0530

    http

---
+++ express/app.js
@@ -0,0 +1,16 @@
+const http=require('https');
+
+const address ='Taj Mahal'
+
+const url='https://api.mapbox.com/geocoding/v5/mapbox.places/' + address +
+        '.json?access_token=pk.eyJ1IjoiemVuZ2lkIiwiYSI6ImNrdG9yNTQ3aDBmZXQydXF1OXF1YnNxZTEifQ.KyGlzrlXExC9uksqLyfAuw&limit=1' 
+const req=http.request(url,(res)=>{
+    let allData="";
+    res.on('data',(chunk)=>{
+        allData += chunk.toString();
+        console.log(allData);
+    })
+
+})
+
+req.end()
